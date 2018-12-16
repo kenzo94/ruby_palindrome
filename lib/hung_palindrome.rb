3212a349d7ac44200d1bb80a4d99127acc559693
kenzo94 <hung11194@gmail.com>
2018-12-16 18:03:58 +0100

    test palindrome

--- lib/hung_palindrome.rb
+++ lib/hung_palindrome.rb
@@ -1,6 +1,30 @@
 require "hung_palindrome/version"
 
-module HungPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  #Returns the letters in the String
+  def letters
+    the_letters = []
+
+    for i in 0..self.length - 1 do
+      if self[i].match(/[a-zA-Z]/)
+        the_letters << self[i]
+    end
+  end
+  the_letters.join
+end
+
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end
+
 end
--- test/hung_palindrome_test.rb
+++ test/hung_palindrome_test.rb
@@ -1,11 +1,25 @@
 require "test_helper"
 
 class HungPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HungPalindrome::VERSION
-  end
 
-  def test_it_does_something_useful
-    assert false
-  end
+    def test_non_palindrome
+      refute  "apple".palindrome?
+    end
+
+    def test_literal_palindrome
+      assert "racecar".palindrome?
+    end
+
+    def test_mixed_case_palindrome
+      assert "RaceCar".palindrome?
+    end
+
+    def test_palindrome_with_punctuation
+      assert "Madam, I'm Adam.".palindrome?
+    end
+
+    # assert_equal <expected>, <actual>
+    def test_letters
+      assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+    end
 end
